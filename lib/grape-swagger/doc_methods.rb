8778dcfdf6670e3e59c2a328b8f3127e9c0ec2aa
Dan Bradbury <dan.luckydaisy@gmail.com>
2016-01-08 11:44:35 -0800

    add array type

--- lib/grape-swagger/doc_methods.rb
+++ lib/grape-swagger/doc_methods.rb
@@ -85,6 +85,8 @@ module GrapeSwagger
                           'double'
                         when 'Symbol'
                           'string'
+                        when 'Array'
+                          'array'
                         when /^\[(?<type>.*)\]$/
                           items[:type] = Regexp.last_match[:type].downcase
                           if PRIMITIVE_MAPPINGS.key?(items[:type])
